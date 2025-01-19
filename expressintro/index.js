e66a1400b655e2cab2fc1122e42dec72e186fe68
sehgaldheeraj <dheerajsehgal0012@gmail.com>
2025-01-19 22:28:34 +0530

    till week 2

---
+++ expressintro/index.js
@@ -0,0 +1,42 @@
+//importing the dependency
+const express = require("express");
+//creating a server instance
+const app = express();
+//localhost:3000
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+}); //args - route, cb fn
+
+//localhost:3000/home
+app.get("/home", (req, res) => {
+  res.send("Hi from Home");
+});
+
+//POST ex: Creating a new user
+/**
+ * 1. Take Data from request
+ * 2. Data validation
+ * 3. Add user to DB
+ */
+users = [];
+app.post("/users", (req, res) => {
+  const { name, age, email, password, phone } = req.body; // const name = req.body.name;
+  if (!name || !email || !password) {
+    res.status(404).send({ message: "Please fill the required fields" });
+  }
+  users.push(req.body);
+  res
+    .status(201)
+    .send({ users: users, message: "account created successfully" });
+});
+//started server as a process
+app.listen(3000, () => {
+  console.log("Server started successfully");
+});
+
+//CRUD Operations
+/**
+ * CREATE: POST
+ */
+
+//npm install nodemon --save-dev    :saves as dev dependency
